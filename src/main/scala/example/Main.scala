15cb5c8e0992e673e97d404e115b0620effd75ac
Mario Galic <mario.galic@guardian.co.uk>
2019-04-10 15:08:44 +0100

    Iterate through the file

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -6,26 +6,27 @@ import scala.io.Source
 import scala.collection.JavaConverters._
 
 object Main extends App {
-  val lines = Source.fromFile("file.txt").getLines
-
   val props = new Properties
   props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner")
   // example customizations (these are commented out but you can uncomment them to see the results
   // customize fine grained ner
   // props.setProperty("ner.fine.regexner.mapping", "example.rules");
   props.setProperty("ner.fine.regexner.ignorecase", "true")
-//  props.setProperty("ner.additional.regexner.mapping", "ignorecase=true,example_one.rules;example_two.rules")
+  //  props.setProperty("ner.additional.regexner.mapping", "ignorecase=true,example_one.rules;example_two.rules")
   props.setProperty("ner.additional.regexner.mapping", "sanitizer.rules")
   props.setProperty("ner.additional.regexner.ignorecase", "true")
-
   val pipeline = new StanfordCoreNLP(props)
+  val lines = Source.fromFile("file.txt").getLines
 
-  val line = List("E11 mario@gu.com heloo world")
-  val doc = new CoreDocument(line.head)
-  pipeline.annotate(doc)
-  val rawLine = doc.tokens.asScala.toList.map(_.word)
-  doc.entityMentions().asScala.toList.foreach(em => println(s"${em.text} : ${em.entityType()}"))
-  val piiLine = doc.entityMentions().asScala.toList.map(_.text)
-  println(rawLine.diff(piiLine))
+  lines.foreach { rawLine =>
+    val doc = new CoreDocument(rawLine)
+    pipeline.annotate(doc)
+    val tokenizedRawLine = doc.tokens.asScala.toList.map(_.word)
+    doc.entityMentions().asScala.toList.foreach(em => println(s"${em.text} : ${em.entityType()}"))
+    val pii = doc.entityMentions().asScala.toList.map(_.text)
+
+    println("Sanitised:")
+    println(tokenizedRawLine.diff(pii))
+  }
 }
 

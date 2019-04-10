669f3e75311f5bbc5936b7c0d84afcdf827d085b
Mario Galic <mario.galic@guardian.co.uk>
2019-04-10 16:08:59 +0100

    Remove println from iteration

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -6,6 +6,7 @@ import scala.io.Source
 import scala.collection.JavaConverters._
 
 object Main extends App {
+  val startTime = System.currentTimeMillis
   val props = new Properties
   props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner")
   // example customizations (these are commented out but you can uncomment them to see the results
@@ -16,17 +17,21 @@ object Main extends App {
   props.setProperty("ner.additional.regexner.mapping", "sanitizer.rules")
   props.setProperty("ner.additional.regexner.ignorecase", "true")
   val pipeline = new StanfordCoreNLP(props)
-  val lines = Source.fromFile("file.txt").getLines
+  val lines = Source.fromFile("file.txt").getLines.filterNot(_.isEmpty)
 
-  lines.foreach { rawLine =>
+  val sanitisedLines = lines.flatMap { rawLine =>
     val doc = new CoreDocument(rawLine)
     pipeline.annotate(doc)
     val tokenizedRawLine = doc.tokens.asScala.toList.map(_.word)
-    doc.entityMentions().asScala.toList.foreach(em => println(s"${em.text} : ${em.entityType()}"))
+//    doc.entityMentions().asScala.toList.foreach(em => println(s"${em.text} : ${em.entityType()}"))
     val pii = doc.entityMentions().asScala.toList.map(_.text)
-
-    println("Sanitised:")
-    println(tokenizedRawLine.diff(pii))
+    tokenizedRawLine.diff(pii)
   }
+
+  println("Last sanitised line:")
+  println(sanitisedLines.toList.last)
+
+  val estimatedTime = System.currentTimeMillis - startTime
+  println(s"Time = ${estimatedTime/1000}")
 }
 
